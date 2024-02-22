26a0e3a27ba8c166a706677a08ae85b433c98a62
khokiat <kho_kw@hotmail.com>
2024-02-22 23:35:45 +0700

    update

--- dags/kafka-stream.py
+++ dags/kafka-stream.py
@@ -26,9 +26,15 @@ def format_data(res):
 
 def stream_data():
     import json
+    from kafka import KafkaProducer
+    import time
+
     res = get_data()
     res = format_data(res)
-    print(json.dumps(res, indent=3)) ##Use json dump to change from dict to json
+    #print(json.dumps(res, indent=3)) ##Use json dump to change from dict to json
+    
+    producer = KafkaProducer(bootstrap_servers=['localhost:29092'], max_block_ms= 5000)
+    producer.send('user_created', json.dumps(res).encode('utf-8'))
 
 with DAG('User_automation',
          default_args=default_args,
@@ -39,5 +45,3 @@ with DAG('User_automation',
         task_id='stream_data_from_api',
         python_callable=stream_data
     )
-
-stream_data();
